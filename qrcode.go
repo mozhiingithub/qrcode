e8a1132ff9f8c12d9f82c222f3b4b18a0b643165
mo <245994626@qq.com>
2020-04-07 12:00:06 +0800

    优化图片弹出机制。

--- qrcode.go
+++ qrcode.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"os/exec"
 
 	qrcode "github.com/skip2/go-qrcode"
 )
 
+const imgName = "qr.png"
+
 func main() {
 	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Print("Input text:")
 	input, _ := inputReader.ReadString('\n')
 	input = input[:len(input)-1]
-	qrcode.WriteFile(input, qrcode.Medium, 256, "qr.png")
-
+	qrcode.WriteFile(input, qrcode.Medium, 256, imgName)
+	exec.Command("xdg-open", imgName).Start()
 }
